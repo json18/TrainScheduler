4a058b60009e5914f662ba345cd086b75ed2f599
Jessica Son <jessicason@Jessicas-MacBook-Pro.local>
2018-04-08 12:42:12 -0700

    Bootstrap partially completed

---
+++ assets/javascript/app.js
@@ -0,0 +1,13 @@
+<script>
+  var config = {
+    apiKey: "AIzaSyCqiJsVN5TnsTDgpMGy2u0AjTolG0bjyt8",
+    authDomain: "train-scheduler-7da3a.firebaseapp.com",
+    databaseURL: "https://train-scheduler-7da3a.firebaseio.com",
+    projectId: "train-scheduler-7da3a",
+    storageBucket: "train-scheduler-7da3a.appspot.com",
+    messagingSenderId: "468168857196"
+  };
+  firebase.initializeApp(config);
+</script>
+
+var 
